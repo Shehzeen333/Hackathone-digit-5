395eefab8a5aee780c9e5fdb1e96cda935317886
Shehzeen333 <prinxess.sheza333@gmail.com>
2024-12-08 20:47:46 +0500

    frst commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import BlueHeader from "../app/components/blue-header";
+import Carausel from "../app/components/caraousel";
+import Editors from "../app/components/editorspick";
+// import Footer from "./components/footer";
+import GreenDiv from "../app/components/green-div";
+import Navbar from "../app/components/navbar";
+import ProductCard from "../app/components/products-card";
+import Whitediv from "../app/components/white-dic";
+
+export default function Home(){
+  return(
+    <div>
+      <BlueHeader/>
+      <Navbar/>
+      <Carausel/>
+      <Editors/>
+      <ProductCard/>
+      <GreenDiv/>
+      <Whitediv/>
+      {/* <Footer/> */}
+           
+    </div>
+  )
+}
