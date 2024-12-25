906d5266f01d39cd3b600e68a46704c207bb8d3a
Shehzeen <159651332+Shehzeen333@users.noreply.github.com>
2024-12-25 21:50:36 +0500

    Add files via upload

---
+++ src/app/piricing/page (1).tsx
@@ -0,0 +1,16 @@
+import ContactUs from "../components/contact-components/contactus"
+import Office from "../components/contact-components/office"
+import Header from '../components/shop-components/header'
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <>
+        <Header />
+        <ContactUs />
+        <Office />
+    </>
+  )
+}
+
+export default ContactPage
